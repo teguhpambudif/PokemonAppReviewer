a18d68991c86b6edb2a74966c9f6a262ed209574
teguhpambudif <teguhpambudi1251@gmail.com>
2023-02-09 01:12:32 +0700

    edit model, synchronizing with seed file

--- PokemonAppReviewer/Models/Owner.cs
+++ PokemonAppReviewer/Models/Owner.cs
@@ -3,7 +3,8 @@ namespace PokemonAppReviewer.Models;
 public class Owner
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string Gym { get; set; }
     public Country Country { get; set; } // because it is only contain one country assosiated with it
     public ICollection<PokemonOwner> PokemonOwners { get; set; }
--- PokemonAppReviewer/Models/Pokemon.cs
+++ PokemonAppReviewer/Models/Pokemon.cs
@@ -4,7 +4,7 @@ public class Pokemon
 {
     public int Id { get; set; }
     public string Name { get; set; }
-    public DateTime Birthdate { get; set; }
+    public DateTime BirthDate { get; set; }
     public ICollection<Review> Reviews { get; set; } // because it is many Review assosiated with it then using Icollec, it is almost the same as a list
     public ICollection<PokemonOwner> PokemonOwners { get; set; }
     public ICollection<PokemonCategory> PokemonCategories { get; set; }
