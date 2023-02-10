7720599fb88469650ea54ad3ac760b72d2e51fb0
teguhpambudif <teguhpambudi1251@gmail.com>
2023-02-11 01:08:02 +0700

    add pokemon repository

---
+++ PokemonAppReviewer/Repository/PokemonRepository.cs
@@ -0,0 +1,18 @@
+using PokemonAppReviewer.Data;
+using PokemonAppReviewer.Models;
+
+namespace PokemonAppReviewer.Repository;
+// in repository we just do database calls. if it's not a db calls then it will be a "service" and not a "repository"
+public class PokemonRepository
+{
+    private readonly DataContext _context;
+    public PokemonRepository(DataContext context)
+    {
+        _context = context;
+    }
+
+    public ICollection<Pokemon> GetPokemons()
+    {
+        return _context.Pokemon.OrderBy(p => p.Id).ToList();
+    }
+}
