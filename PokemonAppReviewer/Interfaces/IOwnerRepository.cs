e30f1c6ae14efe21d5c95feebea23177943cba8d
teguhpambudif <teguhpambudi1251@gmail.com>
2023-02-12 17:00:30 +0700

    add owner iface & repo

---
+++ PokemonAppReviewer/Interfaces/IOwnerRepository.cs
@@ -0,0 +1,12 @@
+using PokemonAppReviewer.Models;
+
+namespace PokemonAppReviewer.Interfaces;
+
+public interface IOwnerRepository
+{
+    bool OwnerExists(int ownerId);
+    ICollection<Owner> GetOwners();
+    Owner GetOwner(int ownerId);
+    ICollection<Owner> GetOwnerOfAPokemon(int pokeId);
+    ICollection<Pokemon> GetPokemonByOwner(int ownerId);
+}
---
+++ PokemonAppReviewer/Repository/OwnerRepository.cs
@@ -0,0 +1,42 @@
+using AutoMapper;
+using PokemonAppReviewer.Data;
+using PokemonAppReviewer.Interfaces;
+using PokemonAppReviewer.Models;
+
+namespace PokemonAppReviewer.Repository;
+
+public class OwnerRepository : IOwnerRepository
+{
+    private readonly DataContext _context;
+    private readonly IMapper _mapper;
+
+    public OwnerRepository(DataContext context,IMapper mapper)
+    {
+        _context = context;
+        _mapper = mapper;
+    }
+    public bool OwnerExists(int ownerId)
+    {
+        return _context.Owners.Any(o=>o.Id==ownerId);
+    }
+
+    public ICollection<Owner> GetOwners()
+    {
+        return _context.Owners.OrderBy(o=>o.Id).ToList();
+    }
+
+    public Owner GetOwner(int ownerId)
+    {
+        return _context.Owners.Where(o=>o.Id==ownerId).FirstOrDefault();
+    }
+
+    public ICollection<Owner> GetOwnerOfAPokemon(int pokeId)
+    {
+        return _context.PokemonOwners.OrderBy(po=>po.Pokemon.Id==pokeId).Select(o=>o.Owner).ToList();
+    }
+
+    public ICollection<Pokemon> GetPokemonByOwner(int ownerId)
+    {
+        return _context.PokemonOwners.OrderBy(o=>o.Owner.Id==ownerId).Select(p=>p.Pokemon).ToList();
+    }
+}
